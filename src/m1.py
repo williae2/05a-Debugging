f2e910ed917f8d27db3e710b8fd3fb652fa3f3f6
williae2 <williae2@rose-hulman.edu>
2018-09-17 16:22:21 -0400

    Debugged

--- src/m1.py
+++ src/m1.py
@@ -309,7 +309,7 @@ def broken_6(n):
 
 
 # ----------------------------------------------------------------------
-# TODO: 9. Follow the INSTRUCTIONS AT THE TOP OF THIS MODULE
+# DONE: 9. Follow the INSTRUCTIONS AT THE TOP OF THIS MODULE
 #          to correct the mistake(s) in the following function.
 # ----------------------------------------------------------------------
 def broken_7(n, point, length, distance_between_lines, window):
